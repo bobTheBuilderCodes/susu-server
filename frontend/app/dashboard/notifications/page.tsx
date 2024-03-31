e430f6c0b14aedbc3193d5a0b9687624ea536a18
Sam Robert <91603698+bobTheBuilderCodes@users.noreply.github.com>
2024-03-31 08:15:25 +0000

    Update page.tsx

--- frontend/app/dashboard/notifications/page.tsx
+++ frontend/app/dashboard/notifications/page.tsx
@@ -6,7 +6,7 @@ const NotificationsTab = () => {
   return (
     <div className='flex bg-gray-100 justify-center items-center flex-col h-screen'>
       <h1 className='text-2xl font-bold text-gray-700'>No Notifications</h1>
-      <p>For now, let's partyyy 🚀 </p>
+      <p>For now, let us partyyy 🚀 </p>
 
       <Link href={'/'} className='bg-black text-white mt-8 py-5 rounded-lg px-12'>Back to Home Page</Link>
     </div>
